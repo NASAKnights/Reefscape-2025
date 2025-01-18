d288096c6deeb4546cc81872699846d7e08b977f
Dakota Thomas <spykid31607@gmail.com>
2025-01-18 16:59:28 -0500

    Added Reset Command and L3SequenceCommand Test

---
+++ src/main/cpp/commands/Reset.cpp
@@ -0,0 +1,23 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include "commands/Reset.h"
+
+Reset::Reset() {
+  // Use addRequirements() here to declare subsystem dependencies.
+}
+
+// Called when the command is initially scheduled.
+void Reset::Initialize() {}
+
+// Called repeatedly when this Command is scheduled to run
+void Reset::Execute() {}
+
+// Called once the command ends or is interrupted.
+void Reset::End(bool interrupted) {}
+
+// Returns true when the command should end.
+bool Reset::IsFinished() {
+  return false;
+}
