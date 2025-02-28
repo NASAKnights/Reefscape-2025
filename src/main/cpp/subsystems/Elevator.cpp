307d30073be75c0a1935e119f7f8b66f50b94815
rgage <rob@gagelogic.com>
2025-02-27 20:40:56 -0500

    adding COMFIT sensor

--- src/main/cpp/subsystems/Elevator.cpp
+++ src/main/cpp/subsystems/Elevator.cpp
@@ -28,6 +28,7 @@ Elevator::Elevator()
                             ElevatorConstants::kFFkV, ElevatorConstants::kFFkA},
       m_encoderLeft{m_motorLeft.GetEncoder()},
       m_encoderRight{m_motorRight.GetEncoder()},
+      m_candi{14}, // TODO may need canbus name
       m_elevatorSim(frc::DCMotor::NEO(ElevatorConstants::kNumMotors), ElevatorConstants::kElevatorGearing,
                     ElevatorConstants::kCarriageMass, ElevatorConstants::kElevatorDrumRadius,
                     ElevatorConstants::simLowerLimit, ElevatorConstants::simUpperLimit, false, 0_m,
@@ -48,9 +49,9 @@ Elevator::Elevator()
     m_StateLog = wpi::log::IntegerLogEntry(log, "/Elevator/State");
     m_MotorCurrentLog = wpi::log::DoubleLogEntry(log, "/Elevator/MotorCurrent");
     m_MotorVoltageLog = wpi::log::DoubleLogEntry(log, "/Elevator/MotorVoltage");
-
-    m_holdHeight = 0.0;
     m_ElevatorState = ElevatorConstants::ElevatorState::DISABLED;
+    m_goal = 0.0_m;
+    m_heightCorrection = 0.0;
 }
 
 void Elevator::HoldPosition()
@@ -77,6 +78,21 @@ void Elevator::SetHeight(double height)
     }
 }
 
+// - drive the elevator down until encoder resets and does not change further
+// - slowly drive the elevator up to cal_height
+// - record difference of the unscaled encoder height and the unscaled
+//   hall sensor height as the correction (under or over tensioned cable)
+void Elevator::Calibrate(double cal_height)
+{
+    if (m_ElevatorState != ElevatorConstants::ElevatorState::DISABLED &&
+        m_ElevatorState != ElevatorConstants::ElevatorState::START_HOLD &&
+        m_ElevatorState != ElevatorConstants::ElevatorState::START_MOVE)
+    {
+        m_ElevatorState = ElevatorConstants::START_CAL;
+        m_goal = units::meter_t{cal_height};
+    }
+}
+
 /*
 void ElevatorSubsystem::SetSpeed(double speed)
 {
@@ -94,25 +110,52 @@ double Elevator::GetHeight()
         // frc::SmartDashboard::PutBoolean("isSim", true);
         return m_elevatorSim.GetPosition().value();
     }
-    // auto gearedVal = ( / ElevatorConstants::kElevatorGearing)*;
+    // average the left and right encoder distance to get the encoder estimated height
+    double encoder_height = GetEncoderHeight() - m_heightCorrection;
+
+    if (ElevatorConstants::kDisableHallSensor)
+        return encoder_height;
+
+    // TODO detect a fault state if we definitely should be within range
+    // of a magnet, but we aren't OR if we definitely should NOT be within
+    // range and we are.  Condition should persist for X milliseconds.
+
+    // using the encoder estimated height, get the height from the hall sensor
+    double hall_height = GetHallHeight(encoder_height);
+    if (hall_height < -999.0)
+        return encoder_height;
+    // TODO smooth out the transition by using a weighted average
+    return hall_height + m_heightCorrection;
+}
+
+double Elevator::GetEncoderHeight()
+{
+    // average the left and right encoder distance to get the encoder estimated height
     return ((GetEncoderDistance(m_encoderLeft) + GetEncoderDistance(m_encoderRight)) / 2.0).value();
+}
+
+double Elevator::GetEncoderVelocity()
+{
+    // average the two encoder velocities (returns rpm)
+    double rpm = (m_encoderLeft.GetVelocity() + m_encoderRight.GetVelocity()) / 2.0;
+    return rpm * 60.0 * 2.0 * std::numbers::pi * ElevatorConstants::kElevatorDrumRadius.value() / ElevatorConstants::kElevatorGearing;
 }
 
 units::meter_t Elevator::GetMeasurement()
 {
     return units::meter_t{GetHeight()};
 }
-/*
-bool ElevatorSubsystem::CheckGoal()
-{
-    return m_ElevatorState == ElevatorConstants::ElevatorState::HOLD;
-}
-*/
+
 void Elevator::printLog()
 {
-    frc::SmartDashboard::PutNumber("/Elevator/ELEVATOR_ENC_ABS", GetMeasurement().value());
+    double encoder_height = GetEncoderHeight();
+    double hall_height = GetHallHeight(encoder_height);
+    frc::SmartDashboard::PutNumber("/Elevator/ELEVATOR_HEIGHT", GetMeasurement().value());
     frc::SmartDashboard::PutNumber("/Elevator/ELEVATOR_ENC_ABS_LEFT", m_encoderLeft.GetPosition());
     frc::SmartDashboard::PutNumber("/Elevator/ELEVATOR_ENC_ABS_RIGHT", m_encoderRight.GetPosition());
+    frc::SmartDashboard::PutNumber("/Elevator/ELEVATOR_ENC_HEIGHT", encoder_height);
+    frc::SmartDashboard::PutNumber("/Elevator/ELEVATOR_HALL_PWM", m_hall_pwm);
+    frc::SmartDashboard::PutNumber("/Elevator/ELEVATOR_HALL_HEIGHT", hall_height);
     frc::SmartDashboard::PutNumber("/Elevator/elevatorGoal_POS", m_controller.GetGoal().position.value());
     frc::SmartDashboard::PutNumber("/Elevator/ELEVATOR_setpoint",
                                    m_controller.GetSetpoint().position.value());
@@ -122,15 +165,143 @@ void Elevator::printLog()
     m_MotorCurrentLog.Append((m_motorLeft.GetOutputCurrent() + m_motorRight.GetOutputCurrent()) / 2.0);
     m_MotorVoltageLog.Append((m_motorLeft.GetAppliedOutput() + m_motorRight.GetAppliedOutput()) / 2.0);
 }
-/*
-void ElevatorSubsystem::Periodic()
+
+// returns the height of the elevator as determined by the hall sensor
+// NOTE returns -1000.0 if no data is available, -2000.0 if fault is detected
+double Elevator::GetHallHeight(double height_estimate)
 {
-    if constexpr (frc::RobotBase::IsTeleop())
+    // find the closest magnet holder using the height estimate
+    double dist;
+    int index = -1;
+    double mindist = 1000.0;
+    for (int i = 0; i < ElevatorConstants::kHallMagnetHolderCount; i++)
     {
-        TeleopPeriodic();
+        dist = std::abs(height_estimate - ElevatorConstants::kHallMagnetHeights[i]);
+        if (dist < mindist)
+        {
+            mindist = dist;
+            index = i;
+        }
+    }
+    double position_estimate = (height_estimate - ElevatorConstants::kHallMagnetHeights[index]) /
+                               ElevatorConstants::kHallStageFactor;
+    int magnet_count = ElevatorConstants::kHallMagnetCounts[index];
+    double position = GetHallPosition(position_estimate, magnet_count);
+    if (position < -999.0)
+        return position;
+    else
+        return ElevatorConstants::kHallMagnetHeights[index] +
+               ElevatorConstants::kHallStageFactor * position;
+}
+
+// returns the position of the magnet holder center relative to the hall sensor center in meters
+// position_estimate is used to determine the integer part of the mag field rotation
+// position_estimate must be accurate to within +/-0.75 inches (0.5 rotation of the mag field)
+// NOTE returns -1000.0 if no data is available, -2000.0 if sensor fault is detected
+double Elevator::GetHallPosition(double position_estimate, int magnet_count)
+{
+    // get the raw hall pwm value
+    double pwm = m_hall_pwm;
+    // normalize to positive value from 0 to 1.0 in case candi is counting revolutions
+    pwm = (pwm < 0 ? -1 : 1) * pwm;
+    pwm = pwm - std::trunc(pwm);
+    // pwm < 0.025 or pwm > 0.95 represents fault
+    // 0.25 < pwm < 0.09 represents no data yet
+    // 0.91 < pwm < 0.95 indeterminate, ignore
+    if (pwm < 0.25 or pwm > 0.95)
+        return -2000.0;
+    if (pwm < 0.09 || pwm > 0.91)
+        return -1000.0;
+    // convert the pwm duty cycle to an angle in rotations
+    double rot = (pwm - 0.1) / 0.8;
+    // clamp rot to 0 to 1
+    if (rot < 0.0)
+        rot = 0.0;
+    if (rot > 1.0)
+        rot = 1.0;
+
+    // estimate the angle from the position estimate using the inverse of the piecewise linear function
+    double rot_estimate;
+    switch (magnet_count)
+    {
+    case 4:
+        // if the estimated position is outside the interpolation range, return no data
+        if (position_estimate < ElevatorConstants::kHall4PwlPosition[0] ||
+            position_estimate > ElevatorConstants::kHall4PwlPosition[ElevatorConstants::kHallPwlPoints - 1])
+            return -1000.0;
+        rot_estimate = InterpolatePWL(ElevatorConstants::kHall4PwlPosition,
+                                      ElevatorConstants::kHall4PwlAngle,
+                                      ElevatorConstants::kHallPwlPoints, position_estimate);
+        break;
+    default:
+        // if the estimated position is outside the interpolation range, return no data
+        if (position_estimate < ElevatorConstants::kHall2PwlPosition[0] ||
+            position_estimate > ElevatorConstants::kHall2PwlPosition[ElevatorConstants::kHallPwlPoints - 1])
+            return -1000.0;
+        rot_estimate = InterpolatePWL(ElevatorConstants::kHall2PwlPosition,
+                                      ElevatorConstants::kHall2PwlAngle,
+                                      ElevatorConstants::kHallPwlPoints, position_estimate);
+        break;
+    }
+    // use the estimated angle to adjust the measured angle beyond a single rotation
+    rot += std::trunc(rot_estimate);
+    if (rot - rot_estimate > 0.5)
+        rot -= 1.0;
+    else
+    {
+        if (rot - rot_estimate < -0.5)
+            rot += 1.0;
+    }
+
+    // interpolate the piecewise linear function at the angle to find the final position
+    switch (magnet_count)
+    {
+    case 4:
+        // if the estimated position is outside the interpolation range, return no data
+        if (rot < ElevatorConstants::kHall4PwlAngle[0] ||
+            rot > ElevatorConstants::kHall4PwlAngle[ElevatorConstants::kHallPwlPoints - 1])
+            return -1000.0;
+        return InterpolatePWL(ElevatorConstants::kHall4PwlAngle,
+                              ElevatorConstants::kHall4PwlPosition,
+                              ElevatorConstants::kHallPwlPoints, rot);
+        break;
+    default:
+        // if the estimated position is outside the interpolation range, return no data
+        if (rot < ElevatorConstants::kHall2PwlAngle[0] ||
+            rot > ElevatorConstants::kHall2PwlAngle[ElevatorConstants::kHallPwlPoints - 1])
+            return -1000.0;
+        return InterpolatePWL(ElevatorConstants::kHall2PwlAngle,
+                              ElevatorConstants::kHall2PwlPosition,
+                              ElevatorConstants::kHallPwlPoints, rot);
+        break;
     }
 }
-*/
+
+// interpolate the piecewise linear function at x given the break points xs, ys
+// result will be extrapolated if outside range of xs
+double Elevator::InterpolatePWL(const double *xs, const double *ys, int count, double x)
+{
+    double dx;
+    double dy;
+
+    for (int i = 0; i < count - 1; i++)
+    {
+        if (x <= xs[i + 1] || i == count - 2)
+        {
+            dx = xs[i + 1] - xs[i];
+            dy = ys[i + 1] - ys[i];
+            return ys[i] + dy * (x - xs[i]) / dx;
+        }
+    }
+}
+
+// gets the raw position (duty cycle) from the candi
+double Elevator::GetHallPWM()
+{
+    ctre::phoenix6::StatusSignal<units::angle::turn_t> signal = m_candi.GetPWM1Position(true);
+    return signal.GetValueAsDouble();
+}
+
 void Elevator::Periodic()
 {
     double fb;
@@ -141,6 +312,9 @@ void Elevator::Periodic()
         m_encoderLeft.SetPosition(0.0);
         m_encoderRight.SetPosition(0.0);
     }
+
+    // update the hall sensor raw pwm measurement
+    m_hall_pwm = GetHallPWM();
     switch (m_ElevatorState)
     {
     case ElevatorConstants::DISABLED:
@@ -149,7 +323,8 @@ void Elevator::Periodic()
     case ElevatorConstants::START_HOLD:
         m_controller.SetTolerance(ElevatorConstants::kTolerancePos,
                                   ElevatorConstants::kToleranceVel);
-        m_controller.Reset(units::meter_t{GetHeight()});
+        m_controller.Reset(units::meter_t{GetHeight()},
+                           units::meters_per_second_t{GetEncoderVelocity()});
         m_controller.SetGoal(m_goal);
         m_ElevatorState = ElevatorConstants::HOLDING;
         frc::SmartDashboard::PutString("/Elevator/ElevState", "HOLDING");
@@ -174,7 +349,8 @@ void Elevator::Periodic()
     case ElevatorConstants::START_MOVE:
         m_controller.SetTolerance(ElevatorConstants::kTolerancePos,
                                   ElevatorConstants::kToleranceVel);
-        m_controller.Reset(units::meter_t{GetHeight()});
+        m_controller.Reset(units::meter_t{GetHeight()},
+                           units::meters_per_second_t{GetEncoderVelocity()});
         m_controller.SetGoal(m_goal);
         m_ElevatorState = ElevatorConstants::MOVING;
         frc::SmartDashboard::PutString("/Elevator/ElevState", "MOVING");
@@ -189,7 +365,6 @@ void Elevator::Periodic()
         {
             fb = m_controller.Calculate(units::meter_t{GetHeight()});
             ff = m_feedforwardElevator.Calculate(m_controller.GetSetpoint().velocity);
-            // units::volt_t ff = 0_V;
             v = units::volt_t{fb} + ff;
             if constexpr (frc::RobotBase::IsSimulation())
             {
@@ -204,6 +379,67 @@ void Elevator::Periodic()
             frc::SmartDashboard::PutString("/Elevator/ElevState", "MOVING");
         }
         break;
+    case ElevatorConstants::START_CAL:
+        // start moving at constant power until encoder zeros
+        m_motorLeft.SetVoltage(ElevatorConstants::kCalZeroVoltage);
+        m_motorRight.SetVoltage(ElevatorConstants::kCalZeroVoltage);
+        m_ElevatorState = ElevatorConstants::CAL_MOVE_ZERO;
+        m_lastCalZero = false;
+        m_heightCorrection = 0.0;
+        break;
+    case ElevatorConstants::CAL_MOVE_ZERO:
+        // check if we have two consecutive zero values from encoders
+        if (m_encoderLeft.GetPosition() == 0.0 and m_encoderRight.GetPosition() == 0.0)
+        {
+            if (m_lastCalZero)
+            {
+                m_motorLeft.SetVoltage(0_V);
+                m_motorRight.SetVoltage(0_V);
+                m_controller.SetTolerance(ElevatorConstants::kCalTolerancePos,
+                                          ElevatorConstants::kCalToleranceVel);
+                m_controller.Reset(units::meter_t{GetHeight()},
+                                   units::meters_per_second_t{GetEncoderVelocity()});
+                m_controller.SetGoal(m_goal);
+                m_ElevatorState = ElevatorConstants::CAL_MOVE_HT;
+            }
+            else
+            {
+                m_lastCalZero = true;
+            }
+        }
+        else
+        {
+            m_lastCalZero = false;
+        }
+        break;
+    case ElevatorConstants::CAL_MOVE_HT:
+        if (m_controller.AtGoal())
+        {
+            // TODO calcuate difference of unscaled encoder only height
+            //      and unscaled hall only height, save as correction
+            double encoder_height = GetEncoderHeight();
+            m_heightCorrection = encoder_height / 2.0 - GetHallHeight(encoder_height) / 2.0;
+            m_ElevatorState = ElevatorConstants::ElevatorState::HOLDING;
+            frc::SmartDashboard::PutString("/Elevator/ElevState", "HOLDING");
+        }
+        else
+        {
+            // use encoder height only
+            fb = m_controller.Calculate(units::meter_t{GetEncoderHeight()});
+            ff = m_feedforwardElevator.Calculate(m_controller.GetSetpoint().velocity);
+            v = units::volt_t{fb} + ff;
+            if constexpr (frc::RobotBase::IsSimulation())
+            {
+                m_elevatorSim.SetInputVoltage(v);
+            }
+            m_motorLeft.SetVoltage(v);
+            m_motorRight.SetVoltage(v);
+
+            frc::SmartDashboard::PutNumber("/Elevator/Elev_UO_PID", fb);
+            frc::SmartDashboard::PutNumber("/Elevator/Elev_UO_FF", ff.value());
+            frc::SmartDashboard::PutNumber("/Elevator/Elev_UO_Volt", v.value());
+            frc::SmartDashboard::PutString("/Elevator/ElevState", "MOVING");
+        }
     }
 
     /*
--- src/main/include/subsystems/Elevator.h
+++ src/main/include/subsystems/Elevator.h
@@ -4,6 +4,8 @@
 #include "wpi/DataLog.h"
 #include <ctre/phoenix6/Pigeon2.hpp>
 #include <ctre/phoenix6/TalonFX.hpp>
+#include <ctre/phoenix6/CANdi.hpp>
+#include <ctre/phoenix6/StatusSignal.hpp>
 #include <frc/DutyCycleEncoder.h>
 #include <frc/Encoder.h>
 #include <frc/controller/ElevatorFeedforward.h>
@@ -36,7 +38,10 @@ namespace ElevatorConstants
         START_HOLD,
         HOLDING,
         START_MOVE,
-        MOVING
+        MOVING,
+        START_CAL,
+        CAL_MOVE_ZERO,
+        CAL_MOVE_HT
     };
 
     static constexpr units::meter_t upperLimit = 30_in; // 57
@@ -48,14 +53,18 @@ namespace ElevatorConstants
     static constexpr units::meters_per_second_t kMaxVelocity = 60.0_in / 1_s;                      // 61.55
     static constexpr units::meters_per_second_squared_t kMaxAcceleration = 240.0_in / (1_s * 1_s); // 460_in / (1_s * 1_s);
 
-    static constexpr double kP = 9.0;                                                              // 0.6 - 15
+    static constexpr double kP = 9.0; // 0.6 - 15
 
-    static constexpr double kI = 0.0;                                                              // 0.0
+    static constexpr double kI = 0.0; // 0.0
     static constexpr double kD = 0.0;
     static constexpr units::volt_t kS = 0.2_V; // minimum voltage to move motor
 
     static constexpr units::meter_t kTolerancePos = 0.01_m;
     static constexpr units::meters_per_second_t kToleranceVel = 0.01_mps;
+
+    // tolerance to use during calibration
+    static constexpr units::meter_t kCalTolerancePos = 0.005_m;
+    static constexpr units::meters_per_second_t kCalToleranceVel = 0.005_mps;
 
     static constexpr units::meter_t kEmergencyTolerance = 0.25_in;
 
@@ -83,6 +92,47 @@ namespace ElevatorConstants
     // effective drum radius = radius of first stage * number of stages
     // pulses per rev must be set correctly in the SparkMax encoder - only reports revolutions
     static constexpr units::meter_t kElevatorDrumRadius = 1.432_in;
+
+    // voltage applied during calibration while moving to zero encoder
+    static constexpr auto kCalZeroVoltage = kFFks * -1.5;
+
+    // piecewise linear fit breakpoint count
+    static const int kHallPwlPoints = 10;
+
+    // piecewise linear fit for 2 magnet holders
+    // piecewise linear fit breakpoint field angles in rotations (0 .. 1)
+    static const double kHall2PwlAngle[kHallPwlPoints] = {
+        -0.03386625, 0.00548981, 0.03846759, 0.0993575, 0.16011927,
+        0.82887919, 0.90070398, 0.94538503, 1.00212295, 1.03386625};
+    // piecewise linear fit breakpoint relative positions in meters
+    static const double kHall2PwlPosition[kHallPwlPoints] = {
+        -0.0278375, -0.02339076, -0.02053397, -0.01653541, -0.01346209,
+        0.0130175, 0.01655938, 0.01931709, 0.02409609, 0.02790332};
+
+    // piecewise linear fit for 4 magnet holders
+    // piecewise linear fit breakpoint field angles in rotations (0 .. 1)
+    static const double kHall4PwlAngle[kHallPwlPoints] = {
+        -1.04600337, -1.00715384, -0.94230751, -0.83611974, -0.24959909,
+        0.38566217, 0.8375927, 0.92915047, 0.99704411, 1.04600337};
+    // piecewise linear fit breakpoint relative positions in meters
+    static const double kHall4PwlPosition[kHallPwlPoints] = {
+        -0.04817201, -0.0434131, -0.03794988, -0.03223475, -0.00944449,
+        0.01464383, 0.03235479, 0.03713524, 0.04237394, 0.04810641};
+
+    // factor applied after pwl linear fit to scale values based on elevator stages
+    static const double kHallStageFactor = 2.0;
+
+    // number of magnet holders
+    static const int kHallMagnetHolderCount = 4;
+    // number of magnets in each holder
+    static const int kHallMagnetCounts[kHallMagnetHolderCount] = {2, 4, 2, 2};
+    // position of the center of each holder in the final scaled reference frame
+    // TODO Measure the distance between L0 mag holder and each of L1/L2, L3, and
+    //      L4 mag holders.  Add the offset from zero position to L0 mag holder
+    //      center to each.  Then multiply each by kHallStageFactor.
+    static double kHallMagnetHeights[kHallMagnetHolderCount] = {0.0, 0.5, 1.0, 2.0};
+
+    static const bool kDisableHallSensor = true;
 }
 
 class Elevator : public frc2::SubsystemBase
@@ -100,12 +150,10 @@ public:
     void UseOutput(double output, State setpoint);
     units::meter_t GetMeasurement();
     void HoldPosition();
+    void Calibrate(double cal_height);
     // void           SetSpeed(double speed);
     void SetHeight(double height);
-    // bool           CheckGoal();
     void Periodic();
-    void TeleopInit();
-    void AutonomousInit();
     void Disable();
     bool IsHolding();
     units::meter_t GetEncoderDistance(rev::spark::SparkRelativeEncoder);
@@ -115,8 +163,14 @@ public:
     }
 
 private:
+    double GetEncoderHeight();   // meters
+    double GetEncoderVelocity(); // meters per second
+    double GetHallHeight(double height_estimate);
+    double GetHallPWM();
+    double GetHallPosition(double position_estimate, int magnet_count);
+    double InterpolatePWL(const double *xs, const double *ys, int count, double x);
+
     frc::ProfiledPIDController<units::meter> m_controller;
-    // frc::PIDController m_holdController;
 
     rev::spark::SparkMax m_motorLeft;
     rev::spark::SparkMax m_motorRight;
@@ -132,14 +186,23 @@ private:
     rev::spark::SparkRelativeEncoder m_encoderLeft;
     rev::spark::SparkRelativeEncoder m_encoderRight;
 
+    ctre::phoenix6::hardware::CANdi m_candi;
+
     frc::Timer m_simTimer;
     frc::Timer *m_timer;
 
     frc::sim::ElevatorSim m_elevatorSim;
 
-    double m_holdHeight;
-
     ElevatorConstants::ElevatorState m_ElevatorState;
 
     units::meter_t m_goal;
+
+    // raw hall pwm value, updated in Periodic
+    double m_hall_pwm;
+
+    // indicates zero encoder has been detected
+    // must persist if we are actually at the limit switch
+    bool m_lastCalZero;
+
+    double m_heightCorrection;
 };
