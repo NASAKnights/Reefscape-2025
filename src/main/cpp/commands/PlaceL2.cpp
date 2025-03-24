d33d862763b31faee1e4a8fb46e521ec2e6331c9
Thanco002 <codingminecrafter@gmail.com>
2025-03-24 18:30:18 -0400

    Day 2 Changes

--- src/main/cpp/commands/PlaceL2.cpp
+++ src/main/cpp/commands/PlaceL2.cpp
@@ -11,7 +11,7 @@
 PlaceL2::PlaceL2(Wrist *wrist, Elevator *elevator)
 {
   // AddCommands(frc2::ParallelCommandGroup(SetElevatorHeight{elevator, 0.357}, MoveWristToAngle{wrist, 30.0}));
-  AddCommands(frc2::ParallelCommandGroup(SetElevatorHeight{elevator, 0.267}, MoveWristToAngle{wrist, 30.0}));
+  AddCommands(frc2::ParallelCommandGroup(SetElevatorHeight{elevator, 0.265}, MoveWristToAngle{wrist, 30.0}));
   //,
   // frc2::SequentialCommandGroup(ScoreCoral{m_wrist, m_intakeCoral}, Reset(m_elevator, m_wrist)));
   // TODO set height and angle to correct values
